274fbceda8ee71b3c819cb8b3b2b8b3e3ae9ec2f
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-26 21:07:49 +0500

    Passes id prop

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -4,7 +4,10 @@ import Label from '../Label';
 function Navbar() {
   return (
     <div id="dv-navbar">
-      <Label value="CAVAI" />
+      <Label
+        value="CAVAI"
+        id="label-company"
+      />
     </div>
   );
 }
