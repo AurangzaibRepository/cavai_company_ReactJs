a045489eea1c469ae0930d9e4897675dfc673d68
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-26 20:55:40 +0500

    Renders label

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,11 @@ import React from 'react';
 import Label from '../Label';
 
 function Navbar() {
-  return ();
+  return (
+    <div id="dv-navbar">
+      <Label value="CAVAI" />
+    </div>
+  );
 }
 
 export default Navbar;
