5367a6aadab33ccc0b3285c7dd2b2cd62659afe2
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-26 21:27:25 +0500

    Adds menu links

--- src/components/Navbar/Menu.jsx
+++ src/components/Navbar/Menu.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Menu() {
-
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/products">Products</Link>
+        </li>
+        <li>
+          <Link to="/clients">Clients</Link>
+        </li>
+        <li>
+          <Link to="/gallery">Creative Gallery</Link>
+        </li>
+        <li>
+          <Link to="/resources">Resources</Link>
+        </li>
+        <li>
+          <Link to="/company">Company</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Menu;
