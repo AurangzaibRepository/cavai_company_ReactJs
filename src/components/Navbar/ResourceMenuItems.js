62ed6d04ea275089fda09f0a4186f390737cb086
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-27 21:58:37 +0500

    Creates recourse menu items component

---
+++ src/components/Navbar/ResourceMenuItems.js
@@ -0,0 +1,19 @@
+const ResourceMenuItems = [
+  {
+    id: 1,
+    title: 'Blog',
+    path: '/blog',
+  },
+  {
+    id: 2,
+    title: 'Cavai TV',
+    path: '/cavai-tv',
+  },
+  {
+    id: 3,
+    title: 'Cases',
+    path: '/cases',
+  },
+];
+
+export default ResourceMenuItems;
