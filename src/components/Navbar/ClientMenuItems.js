6c448cb193ad031062e9dbe85fb1e7844e12305d
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-27 20:20:45 +0500

    Creates menu items for clients

---
+++ src/components/Navbar/ClientMenuItems.js
@@ -0,0 +1,14 @@
+const ClientMenuItems = [
+  {
+    id: 1,
+    title: 'Advertisers',
+    path: '/advertisers',
+  },
+  {
+    id: 2,
+    title: 'Publishers',
+    path: '/publishers',
+  },
+];
+
+export default ClientMenuItems;
