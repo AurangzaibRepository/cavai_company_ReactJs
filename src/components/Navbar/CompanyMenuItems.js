bc2d508235022643d3d2c9e7c1f7f23949fe8984
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-27 22:04:46 +0500

    Creates company menu itesm componenr

---
+++ src/components/Navbar/CompanyMenuItems.js
@@ -0,0 +1,19 @@
+const CompanyMenuItems = [
+  {
+    id: 1,
+    title: 'About us',
+    path: '/about-us',
+  },
+  {
+    id: 2,
+    title: 'Our Culture',
+    path: '/culture',
+  },
+  {
+    id: 3,
+    title: 'Sustainability',
+    path: '/sustainability',
+  },
+];
+
+export default CompanyMenuItems;
