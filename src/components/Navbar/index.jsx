0c9fcda3b09764e4fae1f4fabdcdd9004f693778
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-27 22:18:16 +0500

    Renders Link

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,16 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Label from '../Label';
 import Menu from './Menu';
 import './style.css';
 
 function Navbar() {
   return (
     <div id="dv-navbar">
-      <Label
-        value="CAVAI"
-        id="label-company"
-      />
+      <Link to="/" id="label-company">CAVAI</Link>
       <Menu />
     </div>
   );
