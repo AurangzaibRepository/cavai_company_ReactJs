17269d48d34e8dc0994bfdcd1002c38c3629cf7e
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-06-27 20:05:21 +0500

    Creates poduct menu items componen

---
+++ src/components/Navbar/ProductMenuItems.js
@@ -0,0 +1,14 @@
+const ProductMenuItems = [
+  {
+    id: 1,
+    title: 'Cavai Products',
+    path: '/cavai-products',
+  },
+  {
+    id: 2,
+    title: 'Cavai Cloud',
+    path: '/cavai-cloud',
+  },
+];
+
+export default ProductMenuItems;
