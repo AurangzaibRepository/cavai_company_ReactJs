891f658e812583b0ddb8fbbba3b0d925fcb0e9cc
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-09 23:53:23 +0500

    Assigns id

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,9 +4,12 @@ import './style.css';
 function Footer() {
   return (
     <div id="dv-subfooter" className="d-flex justify-content-center">
-      <div>
+      <div className="text-center">
         <div className="color-white">
           Privacy Policy | Terms of use
+        </div>
+        <div id="dv-copyright">
+          Copyright © 2023 Cavai, All Rights Reserved
         </div>
       </div>
     </div>
