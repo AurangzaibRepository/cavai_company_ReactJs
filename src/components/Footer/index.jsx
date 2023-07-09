42b5e4afd2b8f1f340fe004074315812d317cfd6
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-09 23:54:50 +0500

    Specifies class

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,10 +5,10 @@ function Footer() {
   return (
     <div id="dv-subfooter" className="d-flex justify-content-center">
       <div className="text-center">
-        <div className="color-white">
+        <div className="color-white pb-2">
           Privacy Policy | Terms of use
         </div>
-        <div id="dv-copyright">
+        <div id="dv-copyright" className="pt-2">
           Copyright © 2023 Cavai, All Rights Reserved
         </div>
       </div>
