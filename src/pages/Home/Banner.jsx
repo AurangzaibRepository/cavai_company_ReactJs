0eaec800ad43878f1342f4ec8ac1faad52ad31f1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-10 21:13:56 +0500

    Implements prop types validation

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -5,4 +5,9 @@ function Banner({ title, description }) {
   return ();
 }
 
+Banner.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
+
 export default Banner;
