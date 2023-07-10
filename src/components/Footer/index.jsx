b54f125a4f9f59b6ccadef2abed3f8001d5bbbae
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-10 10:17:31 +0500

    Retrieves year dybamically

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './style.css';
 
 function Footer() {
+  const year = new Date().getFullYear();
+
   return (
     <div id="dv-subfooter" className="d-flex justify-content-center">
       <div className="text-center">
@@ -9,7 +11,9 @@ function Footer() {
           Privacy Policy | Terms of use
         </div>
         <div id="dv-copyright" className="pt-2">
-          Copyright © 2023 Cavai, All Rights Reserved
+          Copyright ©
+          {` ${year} `}
+          Cavai, All Rights Reserved
         </div>
       </div>
     </div>
