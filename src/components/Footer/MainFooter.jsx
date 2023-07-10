0ce9617b46d4e421904ce60cc3d3f8a70af30e1f
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-10 10:32:29 +0500

    Adds divs

--- src/components/Footer/MainFooter.jsx
+++ src/components/Footer/MainFooter.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 function MainFooter() {
-  return ();
+  return (
+    <div id="dv-footer">
+      <div className="row">
+        <div className="col-md-4"></div>
+        <div className="col-md-4"></div>
+        <div className="col-md-4"></div>
+      </div>
+    </div>
+  );
 }
 
 export default MainFooter;
