542bb61abdc0cf1dee79c264494c65d85ddbea08
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-10 17:44:28 +0500

    Imports Routes component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import {
   BrowserRouter,
+  Routes,
+  Route,
 } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
