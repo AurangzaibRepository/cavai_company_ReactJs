8d8cf2a0f4ff00d048657faf7030991aa6c09872
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-10 22:16:48 +0500

    Declares state

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,8 @@ import Banner from './Banner';
 import HomeData from '../../data/home_data.json';
 
 function Home() {
+  const [data] = useState(HomeData);
+
   useEffect(() => {
     document.title = `Home - ${process.env.REACT_APP_APP_NAME}`;
   }, []);
