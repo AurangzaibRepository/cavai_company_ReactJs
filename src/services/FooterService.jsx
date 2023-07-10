cc82055b691394d6b97276938106389819cbcc9c
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-10 13:49:54 +0500

    Adds header

--- src/services/FooterService.jsx
+++ src/services/FooterService.jsx
@@ -1,6 +1,11 @@
 const FooterService = {
   getData: async () => {
-    let data = await fetch('data/footer_data.json');
+    let data = await fetch('data/footer_data.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
     data = await data.json();
 
     return data.data;
