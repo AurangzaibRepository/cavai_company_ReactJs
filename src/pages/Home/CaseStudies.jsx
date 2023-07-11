696f57340313878a3391af199f836f35ac48b631
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-11 23:05:34 +0500

    Imports Image component

--- src/pages/Home/CaseStudies.jsx
+++ src/pages/Home/CaseStudies.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
 import PropTypes from 'prop-types';
+import Image from "../../components/Image";
 import Image1 from '../../assets/home/casestudies1.webp';
 import Image2 from '../../assets/home/casestudies2.webp';
 import Image3 from '../../assets/home/casestudies3.webp';
