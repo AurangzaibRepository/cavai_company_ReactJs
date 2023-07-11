86f2527551f710c4c3b08635655279fd21842ca0
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-11 23:25:13 +0500

    Imports Button component

--- src/pages/Home/CaseStudies.jsx
+++ src/pages/Home/CaseStudies.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Image from '../../components/Image';
+import Button from '../../components/Button';
 import Image1 from '../../assets/home/casestudies1.webp';
 import Image2 from '../../assets/home/casestudies2.webp';
 import Image3 from '../../assets/home/casestudies3.webp';
