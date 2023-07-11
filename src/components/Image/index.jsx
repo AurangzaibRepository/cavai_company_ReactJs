a5835dde91c9f3056faed38c3eb85a40e1a32898
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-11 20:31:02 +0500

    Adds component body

--- src/components/Image/index.jsx
+++ src/components/Image/index.jsx
@@ -1,16 +1,26 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 function Image({
   src,
+  alt,
   id,
   classIdentifier,
 }) {
-  return ();
+  return (
+    <img
+      src={src}
+      alt={alt}
+      id={id}
+      className={classIdentifier}
+    />
+  );
 }
 
 Image.propTypes = {
   src: PropTypes.node.isRequired,
+  alt: PropTypes.string.isRequired,
   id: PropTypes.string,
   classIdentifier: PropTypes.string,
 };
