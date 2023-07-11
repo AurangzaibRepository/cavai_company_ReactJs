2246cb43731a5f4905d4743fab5d04b968ee3866
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-11 23:06:55 +0500

    Renders image component

--- src/pages/Home/CaseStudies.jsx
+++ src/pages/Home/CaseStudies.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
-import Image from "../../components/Image";
+import Image from '../../components/Image';
 import Image1 from '../../assets/home/casestudies1.webp';
 import Image2 from '../../assets/home/casestudies2.webp';
 import Image3 from '../../assets/home/casestudies3.webp';
@@ -20,6 +20,10 @@ function CaseStudies({ items }) {
       <div className="row">
         {items.map((item, index) => (
           <div className="col-md-3">
+            <Image
+              src={imageList[index]}
+              alt="Case Study"
+            />
           </div>
         ))}
       </div>
