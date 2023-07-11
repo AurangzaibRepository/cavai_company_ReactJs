eb6056b5e4cd3e7a0dc533227dd889bd2392d910
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-11 22:45:56 +0500

    Imports images

--- src/pages/Home/CaseStudies.jsx
+++ src/pages/Home/CaseStudies.jsx
@@ -1,10 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from "react";
 import PropTypes from 'prop-types';
+import Image1 from '../../assets/home/casestudies1.webp';
+import Image2 from '../../assets/home/casestudies2.webp';
+import Image3 from '../../assets/home/casestudies3.webp';
+import Image4 from '../../assets/home/casestudies4.webp';
 
 function CaseStudies({ items }) {
   return (
     <div className="dv-section" id="dv-case-studies">
+      <div className="row">
+        {items.map((item, index) => (
+          <div className="col-md-3">
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
