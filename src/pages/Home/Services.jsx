410986af42f3c51ad4efddcd0732278b90d9df1a
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-11 21:01:21 +0500

    Imports paragraph

--- src/pages/Home/Services.jsx
+++ src/pages/Home/Services.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Label from '../../components/Label';
+import Paragraph from '../../components/Paragraph';
 
 function Services({ items }) {
   return (
