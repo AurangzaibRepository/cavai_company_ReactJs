a9ee0286ae04570d30cdcc0fc77a6e3948a26eb5
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-12 23:02:22 +0500

    Renders carousel component

--- src/pages/Home/Partners.jsx
+++ src/pages/Home/Partners.jsx
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import Image from '../../components/Image';
 import Partner1 from '../../assets/home/partner1.webp';
 import Partner2 from '../../assets/home/partner2.webp';
 import Partner3 from '../../assets/home/partner3.webp';
@@ -10,32 +12,49 @@ import Partner6 from '../../assets/home/partner6.webp';
 function Partners() {
   const imageList = [
     {
-        id: 1,
-        image: Partner1,
+      id: 1,
+      image: Partner1,
     },
     {
-        id: 1,
-        image: Partner2,
+      id: 1,
+      image: Partner2,
     },
     {
-        id: 1,
-        image: Partner3,
+      id: 1,
+      image: Partner3,
     },
     {
-        id: 1,
-        image: Partner4,
+      id: 1,
+      image: Partner4,
     },
     {
-        id: 1,
-        image: Partner5,
+      id: 1,
+      image: Partner5,
     },
     {
-        id: 1,
-        image: Partner6,
+      id: 1,
+      image: Partner6,
     },
   ];
 
-  return ();
+  return (
+    <div className="dv-section">
+      <Carousel
+        showStatus={false}
+        autoPlay
+        infiniteLoop
+      >
+        {imageList.map((item) => (
+          <div key={item.id}>
+            <Image
+              src={item.image}
+              alt="Partner"
+            />
+          </div>
+        ))}
+      </Carousel>
+    </div>
+  );
 }
 
 export default Partners;
