c2830d20e30d4d5da58c6f40b4e4f3b4973600c1
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-12 22:46:00 +0500

    Imports images

--- src/pages/Home/Partners.jsx
+++ src/pages/Home/Partners.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import Partner1 from '../../assets/home/partner1.webp';
+import Partner2 from '../../assets/home/partner2.webp';
+import Partner3 from '../../assets/home/partner3.webp';
+import Partner4 from '../../assets/home/partner4.webp';
+import Partner5 from '../../assets/home/partner5.webp';
+import Partner6 from '../../assets/home/partner6.webp';
 
 function Partners() {
   return ();
