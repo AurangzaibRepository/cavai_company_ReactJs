50e9317b0539d1aec0da955ffa821f14a1be30e5
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2023-07-12 20:59:28 +0500

    Imports Link componrnt

--- src/pages/Home/Brand.jsx
+++ src/pages/Home/Brand.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import Label from '../../components/Label';
 import Paragraph from '../../components/Paragraph';
 
